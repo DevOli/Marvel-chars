cbac8e1d811833ec1bde28a3567ec3cb9f2c290a
Fernando-Guerrero-jala <Fernando.Guerrero@jala-foundation.org>
2021-11-23 16:58:08 -0500

    Add Base Hierarchy and Files to Project

---
+++ MarvelCharsApp/MarvelCharsApp/View Models/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  MarvelCharsApp
+//
+//  Created by admin on 11/23/21.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
---
+++ MarvelCharsApp/MarvelCharsApp/Views/DetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.swift
+//  MarvelCharsApp
+//
+//  Created by admin on 11/23/21.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
