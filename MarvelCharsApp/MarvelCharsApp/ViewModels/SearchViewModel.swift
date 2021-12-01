f07ee95ec2093d793feaabba4394197167c41a1c
Oliver Rivas <oliver.rivas@jala-foundation.org>
2021-12-01 16:26:25 -0400

    Add unit testing and modified Api call

--- MarvelCharsApp/MarvelCharsApp/ViewModels/SearchViewModel.swift
+++ MarvelCharsApp/MarvelCharsApp/ViewModels/SearchViewModel.swift
@@ -7,45 +7,49 @@
 
 import Foundation
 class SearchViewModel {
-  private var apiCaller = MarvelAPI()
-  var refreshData = { () -> () in }
-  
-  private var searchedItems: [CharacterModel] = [] {
-    didSet {
-      refreshData()
+    private var apiCaller = MarvelAPI()
+    var refreshData = { () -> () in }
+    
+    private var searchedItems: [CharacterModel] = [] {
+        didSet {
+            refreshData()
+        }
     }
-  }
-  
-  init() {
-    apiCaller.delegate = self
-  }
-  
-  func count() -> Int {
-    return searchedItems.count
-  }
-  
-  func get(byIndex index: Int) -> CharacterModel {
-    return searchedItems[index]
-  }
-  
-  func getAllcharacters() {
-    apiCaller.fetchData()
-  }
-  
-  func getCharacters(byName name:String) {
-    if !name.isEmpty {
-      searchedItems = searchedItems.filter { (character: CharacterModel) -> Bool in
-            return (character.name.lowercased().contains(name.lowercased()))
-      }
-    } else {
-      apiCaller.fetchData()
+    
+    init() {
+        apiCaller.delegate = self
     }
-  }
-  
+    
+    func count() -> Int {
+        return searchedItems.count
+    }
+    
+    func get(byIndex index: Int) -> CharacterModel {
+        return searchedItems[index]
+    }
+    
+    func getAllcharacters() {
+        apiCaller.fetchData()
+    }
+    
+    func getCharacters(byName name:String) {
+        if !name.isEmpty {
+            searchedItems = searchedItems.filter { (character: CharacterModel) -> Bool in
+                return (character.name.lowercased().contains(name.lowercased()))
+            }
+        } else {
+            apiCaller.fetchData()
+        }
+    }
+    
 }
 
 extension SearchViewModel: MarvelAPIDelegate {
-  func didFetchData(heroes: CategoryModel, villains: CategoryModel, antiHeroes: CategoryModel, aliens: CategoryModel, humans: CategoryModel) {
-    searchedItems = heroes.characters + villains.characters + antiHeroes.characters + aliens.characters + humans.characters
-  }
+    func didFetchData(categories: [CategoryModel]) {
+        var charnames: [CharacterModel] = []
+        categories.forEach { category in
+            charnames += category.characters
+        }
+        searchedItems = charnames
+    }
 }
---
+++ MarvelCharsApp/MarvelCharsAppTests/SearchViewModelTests.swift
@@ -0,0 +1,98 @@
+//
+//  SearchViewModelTests.swift
+//  MarvelCharsAppTests
+//
+//  Created by User on 11/30/21.
+//
+
+import XCTest
+@testable import MarvelCharsApp
+
+class SearchViewModelTests: XCTestCase {
+    var search = SearchViewModel()
+    var refreshCallsCount = 0
+    
+    override func setUpWithError() throws {
+        let expectationForFecthAll = self.expectation(description: "Request to API")
+        search.refreshData = {
+            self.refreshCallsCount += 1
+            expectationForFecthAll.fulfill()
+        }
+        search.getAllcharacters()
+        self.waitForExpectations(timeout: 2, handler: nil)
+    }
+
+    override func tearDownWithError() throws {
+        refreshCallsCount = 0
+    }
+    
+    func testGetAllCharacters() throws {
+        let count = self.search.count()
+        XCTAssertEqual(1, self.refreshCallsCount)
+        XCTAssertEqual(15, count)
+    }
+
+    func testCountFunction() throws {
+        let count = search.count()
+        XCTAssertGreaterThan(count, 0)
+    }
+
+    func testGetCharacterByIndex() throws {
+        let items = search.get(byIndex: 0)
+        XCTAssertNotNil(items)
+    }
+
+    func testGetCharacterByCorrectName() throws {
+        let expectationForSearch = self.expectation(description: "Search locally")
+        search.refreshData = {
+            self.refreshCallsCount += 1
+            expectationForSearch.fulfill()
+        }
+        search.getCharacters(byName: "Homem Aranha")
+        self.waitForExpectations(timeout: 2, handler: nil)
+        let count = search.count()
+        XCTAssertEqual(2, refreshCallsCount)
+        XCTAssertGreaterThan(count, 0)
+    }
+
+    func testGetCharacterByInCorrectName() throws {
+        let expectationForSearch = self.expectation(description: "Search locally")
+        search.refreshData = {
+            self.refreshCallsCount += 1
+            expectationForSearch.fulfill()
+        }
+        search.getCharacters(byName: "Wrong Name")
+        self.waitForExpectations(timeout: 2, handler: nil)
+        let count = search.count()
+        XCTAssertEqual(2, refreshCallsCount)
+        XCTAssertEqual(0, count)
+    }
+
+    func testGetCharacterByEmptyName() throws {
+        let expectationForSearch = self.expectation(description: "Search locally")
+        search.refreshData = {
+            self.refreshCallsCount += 1
+            expectationForSearch.fulfill()
+        }
+        search.getCharacters(byName: "")
+        self.waitForExpectations(timeout: 2, handler: nil)
+        let count = search.count()
+        XCTAssertEqual(2, refreshCallsCount)
+        XCTAssertEqual(15, count)
+    }
+}
+
+//Not using mock in this test
+class MockMarvelAPI {
+    
+    var delegate: MarvelAPIDelegate?
+
+    let characterOne = CharacterModel(name: "SpidermanTest", alterEgo: "AlterEgoTest", imagePath: "", biography: "", birth: "", weight: 0, weightUnit: "", height: 0, heightUnit: "", universe: "", force: 0, intelligence: 0, agility: 0, endurance: 0, velocity: 0, movies: [])
+    let characterTwo = CharacterModel(name: "IronManTest", alterEgo: "AlterEgoTest", imagePath: "", biography: "", birth: "", weight: 0, weightUnit: "", height: 0, heightUnit: "", universe: "", force: 0, intelligence: 0, agility: 0, endurance: 0, velocity: 0, movies: [])
+    let characterThree = CharacterModel(name: "DeadPoolTest", alterEgo: "AlterEgoTest", imagePath: "", biography: "", birth: "", weight: 0, weightUnit: "", height: 0, heightUnit: "", universe: "", force: 0, intelligence: 0, agility: 0, endurance: 0, velocity: 0, movies: [])
+    
+    func fetchData() {
+        let categories = [CategoryModel(category: "CategoryTest", characters: [characterOne, characterTwo, characterThree])]
+        self.delegate?.didFetchData(categories: categories)
+    }
+}
