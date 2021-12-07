3d1de3abbd27734cd8715aba1e2cbdf45d8331d3
pablo <pablo.lozada@jala-foundation.org>
2021-12-07 18:36:06 -0400

    Category buttons and main title

---
+++ MarvelCharsApp/MarvelCharsApp/Views/Cells/CategoryButtonCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryButtonCollectionViewCell.swift
+//  MarvelCharsApp
+//
+//  Created by User on 7/12/21.
+//
+
+import UIKit
+
+class CategoryButtonCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var categoryButton: UIButton!
+}
